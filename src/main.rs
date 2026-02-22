2471348c26305c74868c9c638edbf51b52aecf6a
Pascal Kühner <pascal.kuehner@gmx.net>
2026-02-22 12:37:24 +0100

    Uncommenting the non working interrupt

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-mod sensors;
 mod led;
+mod sensors;
 use esp_idf_hal::delay::FreeRtos;
 use esp_idf_hal::peripherals::Peripherals;
 
@@ -13,23 +13,28 @@ fn main() {
 
     let peripherals = Peripherals::take().unwrap();
     let led_pin = peripherals.pins.gpio4;
-    let motion_pin = peripherals.pins.gpio10;
+    let motion_pin = peripherals.pins.gpio5;
 
     let mut led = led::Led::new(led_pin);
-    let mut motion_sensor = sensors::MotionSensor::new(motion_pin);
-
-
+    let motion_sensor = sensors::MotionSensor::new(motion_pin);
 
     loop {
+        //FreeRtos::delay_ms(100);
+        // not detecting falling edges
+        // if motion_sensor.take_changed() {
+        //     log::info!("motion changed, level={}", motion_sensor.is_moving());
+        //     if motion_sensor.is_moving() {
+        //         led.light_on();
+        //     } else {
+        //         led.light_off();
+        //     }
+        // }
+
         FreeRtos::delay_ms(1000);
-        log::info!("Hello, world!");
         if motion_sensor.is_moving() {
             led.light_on();
-        }
-        else{
+        } else {
             led.light_off();
         }
-        log::info!("Hello, world!");
-
     }
 }
--- src/sensors.rs
+++ src/sensors.rs
@@ -1,19 +1,46 @@
-use esp_idf_hal::gpio::{Input, InputPin, PinDriver};
+use std::sync::Arc;
+use core::sync::atomic::{AtomicBool, Ordering};
+use esp_idf_hal::gpio::{Input, InputPin, InterruptType, OutputPin, PinDriver, Pull};
+use esp_idf_sys::EspError;
 
-pub struct MotionSensor<'a, T: InputPin> {
-    motion_detected: bool,
+pub struct MotionSensor<'a, T: InputPin + OutputPin> {
     pin_driver: PinDriver<'a, T, Input>,
+    changed: Arc<AtomicBool>,
 }
 
-impl<'a, T: InputPin> MotionSensor<'a, T> {
+impl<'a, T: InputPin + OutputPin> MotionSensor<'a, T> {
     pub fn new(pin: T) -> Self {
-        Self {
-            motion_detected: false,
+        let mut sensor = Self {
             pin_driver: PinDriver::input(pin).unwrap(),
-        }
+            changed: Arc::new(AtomicBool::new(false)),
+        };
+        
+        sensor.pin_driver.set_pull(Pull::Up).unwrap();
+        sensor.enable_change_interrupt().unwrap();
+
+        sensor
     }
-    pub fn is_moving(&mut self) -> bool {
-        self.motion_detected = self.pin_driver.is_high();
-        return self.motion_detected;
+
+    pub fn is_moving(&self) -> bool {
+        self.pin_driver.is_high()
+    }
+
+    pub fn enable_change_interrupt(&mut self) -> Result<(), EspError> {
+        self.pin_driver.set_interrupt_type(InterruptType::AnyEdge)?;
+
+        let changed = Arc::clone(&self.changed);
+
+        unsafe {
+            self.pin_driver.subscribe(move || {
+                changed.store(true, Ordering::SeqCst);
+            })?;
+        }
+
+        self.pin_driver.enable_interrupt()?;
+        Ok(())
+    }
+
+    pub fn take_changed(&self) -> bool {
+        self.changed.swap(false, Ordering::SeqCst)
     }
 }
